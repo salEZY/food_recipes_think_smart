bda259d961e75aee238b236320dca10caab4dbb0
salEZY <bobicaleks@yahoo.com>
2021-06-19 12:14:15 +0200

    start work on client side

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 const connectToDb = require("./util/db");
 
