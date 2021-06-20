a4371ee151539e8eb09c6059e4ae8b2fff8b60b7
salEZY <bobicaleks@yahoo.com>
2021-06-20 17:03:40 +0200

    add header basic

--- client-recipes/src/components/Header/Header.js
+++ client-recipes/src/components/Header/Header.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Header.css";
 
 const Header = () => {
-  return <header>HEADER</header>;
+  return (
+    <header>
+      <Link to="/">
+        <p>THINK FOOD</p>
+      </Link>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/categories">CATEGORIES</Link>
+          </li>
+          <li>
+            <Link to="/search">SEARCH</Link>
+          </li>
+          <li>
+            <Link to="/meals/:userId">MY MEALS</Link>
+          </li>
+          <li>
+            <Link to="/auth">AUTHENTICATE</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
